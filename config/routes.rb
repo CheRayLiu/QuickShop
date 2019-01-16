cf850b0bf0b5d400d5b815ca18e5898241e3a510
Ray Liu <rayliu109@gmail.com>
2019-01-15 20:24:57 -0500

    Revert Routing change

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount(GraphiQL::Rails::Engine, at: "/", graphql_path: "/graphql")
-  
+  if Rails.env.development?
+    mount(GraphiQL::Rails::Engine, at: "/", graphql_path: "/graphql")
+  end
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
