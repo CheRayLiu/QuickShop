3863b423b5bd51d36982ada7bd213efa46683891
Ray Liu <rayliu109@gmail.com>
2019-01-15 21:48:15 -0500

    added post routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
-class ApplicationController < ActionController::Base
-end
+class ApplicationController < ActionController::API
+    def query
+      result = Schema.execute(
+        params[:query]
+      )
+      render json: result
+    end
+
+    def mutate
+        result = Schema.execute(
+          params[:mutate]
+        )
+        render json: result
+      end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
     mount(GraphiQL::Rails::Engine, at: "/", graphql_path: "/graphql")
   end
   post "/graphql", to: "graphql#execute"
+
+  post '/query' => 'application#query'
+  post '/mutate' => 'application#mutate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
