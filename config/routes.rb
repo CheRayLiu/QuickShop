1c7b22f693843bd5a768d11d0df3df3f1b5c958a
Ray Liu <rayliu109@gmail.com>
2019-01-15 20:11:47 -0500

    Changed routing of graphql

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  if Rails.env.development?
-    mount(GraphiQL::Rails::Engine, at: "/", graphql_path: "/graphql")
-  end
+  mount(GraphiQL::Rails::Engine, at: "/", graphql_path: "/graphql")
+  
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
