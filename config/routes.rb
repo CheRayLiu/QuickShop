a585365982528e2cfdd004ce34f0afec8d88b919
Ray Liu <rayliu109@gmail.com>
2019-01-16 14:33:41 -0500

    Using defauly web process

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   if Rails.env.development?
-    mount(GraphiQL::Rails::Engine, at: "/", graphql_path: "/graphql")
+    mount(GraphiQL::Rails::Engine, at: "/graphql", graphql_path: "/graphql")
   end
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
