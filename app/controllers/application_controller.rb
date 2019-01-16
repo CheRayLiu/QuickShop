7e17769ac1285c54760f82b24f6bf191fd0e646e
Ray Liu <rayliu109@gmail.com>
2019-01-15 22:30:34 -0500

    fixed routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-    def query
-      result = Schema.execute(
-        params[:query]
-      )
-      render json: result
-    end
-
-    def mutate
-        result = Schema.execute(
-          params[:mutate]
-        )
-        render json: result
-      end
-  end
+end
   
